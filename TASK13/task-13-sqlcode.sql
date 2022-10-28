b47e039c7429168940db4bc3322dc808dc23f3e1
falkanx <116848375+falkanx@users.noreply.github.com>
2022-10-28 16:57:22 +0300

    Update task-13-sqlcode.sql

--- TASK13/task-13-sqlcode.sql
+++ TASK13/task-13-sqlcode.sql
@@ -1,29 +1,12 @@
-SELECT employee_id, last_name, salary FROM hr.employees WHERE salary > (SELECT AVG(salary) FROM hr.employees) ORDER BY salary;
-
-SELECT last_name FROM hr.employees WHERE salary > (SELECT MIN(salary) FROM hr.employees WHERE department_id = 60);
-
-SELECT last_name, salary FROM hr.employees WHERE manager_id IN (SELECT employee_id FROM hr.employees WHERE last_name = 'King');
-
 SELECT
-	emp.last_name,
-	emp.department_id,
-	emp.job_id
-FROM
-	hr.employees emp,
-	hr.departments dept,
-	hr.locations loc
-WHERE
-	emp.department_id = dept.department_id
-	AND dept.location_id = loc.location_id
-	AND loc.location_id = 1700;
-
-SELECT employee_id, last_name FROM hr.employees WHERE department_id IN (SELECT department_id FROM hr.employees WHERE last_name LIKE '%u%');
-
-SELECT last_name FROM hr.employees WHERE department_id in
-	(SELECT department_id FROM hr.departments WHERE location_id in
-		(SELECT location_id FROM hr.locations WHERE country_id in
-			(SELECT country_id FROM hr.countries WHERE region_id=
-				(SELECT region_id FROM hr.regions WHERE region_name='Europe')
-			)
-		)
-	)
+	last_name,
+	department_id,
+	salary
+	FROM
+		hr.employees emp
+		WHERE
+		salary> (SELECT round(AVG(salary))
+		FROM hr.employees empl
+		WHERE emp.department_id = empl.department_id
+		GROUP BY department_id
+	);
