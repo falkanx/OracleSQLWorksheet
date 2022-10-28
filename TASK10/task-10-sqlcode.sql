62480b6023496359d4f51e8aa90fbc2dbbb1876e
falkanx <116848375+falkanx@users.noreply.github.com>
2022-10-28 16:15:14 +0300

    Update task-10-sqlcode.sql

--- TASK10/task-10-sqlcode.sql
+++ TASK10/task-10-sqlcode.sql
@@ -1,19 +1,24 @@
-create user FURKAN IDENTIFIED BY FURKAN;
+SELECT last_name || ', ' || job_id as "Employee and Title" FROM HR.EMPLOYEES;
 
-SELECT * FROM ALL_TABLES WHERE OWNER='FURKAN';
+SELECT last_name, salary FROM HR.EMPLOYEES where salary>12000;
 
-ALTER USER FURKAN QUOTA UNLIMITED ON USERS
+SELECT last_name, department_id FROM HR.EMPLOYEES WHERE department_id IN (20,50) ORDER BY last_name asc;
 
-CREATE TABLE FURKAN. DUMP_TEST_FURKAN (A NUMBER, B DATE);
-INSERT INTO FURKAN. DUMP_TEST_FURKAN VALUES (1, SYSDATE);
-INSERT INTO FURKAN. DUMP_TEST_FURKAN VALUES (2, SYSDATE);
-INSERT INTO FURKAN. DUMP_TEST_FURKAN VALUES (3, SYSDATE);
+SELECT last_name, hire_date FROM HR.EMPLOYEES WHERE HIRE_DATE LIKE '%00';
 
-SELECT FROM FURKAN. DUMP TEST FURKAN;
+SELECT last_name FROM HR.EMPLOYEES WHERE last_name LIKE '__a%';
 
-create table FURKAN. DUMP_TEST_FURKAN1 as select * from FURKAN. DUMP_TEST_FURKAN;
-create table FURKAN. DUMP TEST FURKAN2 as select * from FURKAN. DUMP TEST FURKAN;
+SELECT last_name FROM HR.EMPLOYEES WHERE instr(last_name, 'a')>0 AND instr(last_name, 'e')>0;
 
-SELECT * FROM IMPTST. DUMP_TEST_syk;
-SELECT * FROM IMPTST. DUMP_TEST_syk1;
-SELECT * FROM IMPTST. DUMP_TEST_syk2;
+SELECT last_name, job_id, salary FROM HR.EMPLOYEES WHERE job_id='SA_REP' OR job_id='ST_CLERK' AND salary<>2500 AND salary<>3500 and salary<>7000;
+
+SELECT last_name soyad, lpad (salary, 15,'$') AS maas FROM HR.EMPLOYEES;
+
+SELECT last_name soyad, rpad(' ', salary/1000,'*') as EMPLOYYEE_AND_THEIR_SALARIES FROM hr.employees ORDER BY salary DESC;
+
+SELECT last_name || ' earns' || to_char(salary, '$99,999.99') || ' monthly but wants' || to_char(salary*3, '$99,999.99') as "Dream Salaries" FROM hr.employees ORDER BY salary DESC;
+
+SELECT last_name, hire_date, to_char(next_day(add_months(hire_date,6),'Monday'), 'fmDay,"the" Ddspth "of" Month. yyyy') as REVIEW FROM hr.employees ORDER BY hire_date ASC;
+
+SELECT job_id AS "MESLEGI", CASE job_id WHEN 'AD_PRES' THEN 'A' WHEN 'ST_MAN' THEN 'B' WHEN 'IT_PROG' THEN 'C' WHEN 'SA_REP' THEN 'D' WHEN 'ST_CLERK' THEN 'E' ELSE '0' END AS "GRADE" FROM hr.employees;
+
