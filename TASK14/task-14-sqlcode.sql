6d35d7c49603354fed4e73813f2ec22562983c1c
falkanx <116848375+falkanx@users.noreply.github.com>
2022-10-28 16:56:29 +0300

    Update task-14-sqlcode.sql

--- TASK14/task-14-sqlcode.sql
+++ TASK14/task-14-sqlcode.sql
@@ -1,29 +1,98 @@
-SELECT employee_id, last_name, salary FROM hr.employees WHERE salary > (SELECT AVG(salary) FROM hr.employees) ORDER BY salary;
+CREATE TABLE MY_EMPLOYEE (
+ID number (4) not null,
+last_name varchar2(25),
+first_name varchar2(25),
+userid varchar2(8),
+salary number(9,2)
+)
 
-SELECT last_name FROM hr.employees WHERE salary > (SELECT MIN(salary) FROM hr.employees WHERE department_id = 60);
+DESC MY_EMPLOYEE;
 
-SELECT last_name, salary FROM hr.employees WHERE manager_id IN (SELECT employee_id FROM hr.employees WHERE last_name = 'King');
+INSERT INTO MY_EMPLOYEE VALUES (1, 'Patel', 'Ralph', 'rpatel', 895);
+INSERT INTO MY_EMPLOYEE VALUES (2, 'Dancs', 'Betty', 'bdancs', 860);
+INSERT INTO MY_EMPLOYEE VALUES (3, 'Biri', 'Ben', 'bbiri', 1100);
+INSERT INTO MY_EMPLOYEE VALUES (4, 'Newman', 'Chad', 'cnewman', 750);
+INSERT INTO MY_EMPLOYEE VALUES (5, 'Ropeburn', 'Audrey', 'aropebur', 1550);
 
-SELECT
-	emp.last_name,
-	emp.department_id,
-	emp.job_id
-FROM
-	hr.employees emp,
-	hr.departments dept,
-	hr.locations loc
-WHERE
-	emp.department_id = dept.department_id
-	AND dept.location_id = loc.location_id
-	AND loc.location_id = 1700;
+COMMIT
 
-SELECT employee_id, last_name FROM hr.employees WHERE department_id IN (SELECT department_id FROM hr.employees WHERE last_name LIKE '%u%');
+SELECT * FROM MY_EMPLOYEE;
 
-SELECT last_name FROM hr.employees WHERE department_id in
-	(SELECT department_id FROM hr.departments WHERE location_id in
-		(SELECT location_id FROM hr.locations WHERE country_id in
-			(SELECT country_id FROM hr.countries WHERE region_id=
-				(SELECT region_id FROM hr.regions WHERE region_name='Europe')
-			)
-		)
+DELETE FROM MY_EMPLOYEE WHERE ID=5;
+
+UPDATE MY_EMPLOYEE SET SALARY=1000 where SALARY<900;
+
+DELETE FROM MY_EMPLOYEE WHERE ID=2;
+
+INSERT INTO MY_EMPLOYEE VALUES (5, 'Ropeburn', 'Audrey', 'aropebur', 1550);
+
+SAVEPOINT TASK;
+
+DELETE FROM MY_EMPLOYEE;
+
+rollback to savepoint TASK;
+
+CREATE TABLE DEPT (
+ID number (7) NOT NULL PRIMARY KEY,
+name varchar2 (25)
+)
+
+desc DEPT
+
+CREATE TABLE EMP (
+ID number(7),
+last_name varchar2(25),
+first_name varchar2(25),
+dept_id number(7)
+)
+
+ALTER TABLE EMP ADD CONSTRAINT DEPT_FK FOREIGN KEY ("DEPT_ID") REFERENCES "DEPT" ("ID");
+
+desc EMP
+
+ALTER TABLE EMP ADD (COMMISSION number(2, 2))
+
+desc EMP
+
+ALTER TABLE EMP MODIFY (LAST_NAME varchar2(50))
+
+desc EMP
+
+ALTER TABLE EMP DROP COLUMN FIRST_NAME
+
+desc EMP
+
+ALTER TABLE EMP SET UNUSED (DEPT_ID)
+
+desc EMP
+
+ALTER TABLE EMP DROP UNUSED COLUMNS
+
+desc EMP
+
+CREATE TABLE employees2 (
+	emp_id NUMBER (6),
+	first_name VARCHAR2(20),
+	last_name VARCHAR2 (25) NOT NULL,
+	salary NUMBER(8, 2),
+	dept_id NUMBER(4)
 	)
+
+desc EMPLOYEES2
+
+ALTER TABLE EMPLOYEES2 READ ONLY
+
+ALTER TABLE EMPLOYEES2 ADD (JOB_ID varchar2(9))
+
+desc EMPLOYEES2
+
+ALTER TABLE EMPLOYEES2 DROP COLUMN JOB_ID
+
+DROP TABLE EMP;
+
+DROP TABLE DEPT;
+
+DROP TABLE EMPLOYEES2;
+
+
+
